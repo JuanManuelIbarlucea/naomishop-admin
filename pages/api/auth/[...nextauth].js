f9e8ac2ab4a7b89843e3c0a0e99ba4adda906813
JuanManuelIbarlucea <jmibarlucea@gmail.com>
2023-08-06 19:50:20 -0300

    Linted nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
-import NextAuth, { getServerSession } from "next-auth";
+import NextAuth, { getServerSession } from 'next-auth';
 
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/lib/mongodb";
+import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from '@/lib/mongodb';
 
-const adminEmails = ["jmibarlucea@gmail.com"];
+const adminEmails = ['jmibarlucea@gmail.com'];
 
 export const authOptions = {
   providers: [
@@ -29,9 +29,9 @@ export const authOptions = {
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401)
-    res.end()
-    throw "Unauthorized";
+    res.status(401);
+    res.end();
+    throw 'Unauthorized';
   }
 }
 
